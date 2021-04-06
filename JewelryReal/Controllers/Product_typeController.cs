bb423dcd7aa09b800b6364cff0a7551dec5a497c
Chernyshelly <andreycherni@gmail.com>
2021-04-06 21:57:22 +0300

    Added product types and removed id from edit

---
+++ JewelryReal/Controllers/Product_typeController.cs
@@ -0,0 +1,89 @@
+﻿using JewelryReal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JewelryReal.Controllers
+{
+    public class Product_typeController : Controller
+    {
+        private ApplicationContext db;
+        public Product_typeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Product_types()
+        {
+            return View(await db.Product_types.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product_type user)
+        {
+            db.Product_types.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Product_types");
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Product_type user = await db.Product_types.FirstOrDefaultAsync(p => p.TypeID == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Product_type user = await db.Product_types.FirstOrDefaultAsync(p => p.TypeID == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product_type user)
+        {
+            db.Product_types.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Product_types");
+        }
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Product_type user = await db.Product_types.FirstOrDefaultAsync(p => p.TypeID == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Product_type user = await db.Product_types.FirstOrDefaultAsync(p => p.TypeID == id);
+                if (user != null)
+                {
+                    db.Product_types.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Product_types");
+                }
+            }
+            return NotFound();
+        }
+    }
+}
